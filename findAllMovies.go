a147b1f145993b2ad70a86099391dc73c5bc07e8
callankuo <31081668+callankuo@users.noreply.github.com>
2019-04-24 14:28:58 -0700

    Add files via upload

--- findAllMovies.go
+++ findAllMovies.go
@@ -68,7 +68,8 @@ func findAll(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Headers: map[string]string{
-			"Content-Type": "application/json",
+			"Content-Type":                "application/json",
+			"Access-Control-Allow-Origin": "*",
 		},
 		Body: string(response),
 	}, nil
